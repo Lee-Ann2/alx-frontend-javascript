ddd1a31d456018a24a8fdf9876b4e820c0b9ea12
Lee-Ann2 <mfenyanakhazimla17@gmail.com>
2024-12-15 03:14:45 +0200

    Wrote some interfaces

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -15,3 +15,21 @@ const teacher3: Teacher = {
 };
   
   console.log(teacher3);
+
+
+interface Directors {
+    firstName: string;
+    lastName: string;
+    location: string;
+    fullTimeEmployee: Boolean;
+    numberOfReports: number;
+}
+
+const director1: Directors = {
+    firstName: 'John',
+    lastName: 'Doe',
+    location: 'London',
+    fullTimeEmployee: true,
+    numberOfReports: 17,
+  };
+  console.log(director1);
