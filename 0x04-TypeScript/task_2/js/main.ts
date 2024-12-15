d71ae4d46a67295c23a682c477677fc29b4c4ef4
Lee-Ann2 <mfenyanakhazimla17@gmail.com>
2024-12-15 04:37:24 +0200

    Getting there

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -65,3 +65,17 @@ function executeWork(employee: Director | Teacher): void {
 
 executeWork(createEmployee(200));
 executeWork(createEmployee(1000));
+
+type Subjects = 'Math' | 'History';
+
+function teachClass(todayClass: Subjects): string {
+    if (todayClass === 'Math') {
+        return 'Teaching Math';
+    } else if (todayClass === 'History') {
+        return 'Teaching History';
+    }
+    return 'Invalid class';
+}
+
+teachClass('Math');
+teachClass('History');
